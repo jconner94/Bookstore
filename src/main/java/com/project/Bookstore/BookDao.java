d622ea09f9b071b5e1c77ffa02eda1a93362e963
Kevin Jiang <kevinjiang121@gmail.com>
2021-04-28 17:20:01 -0400

    Added some stuff to Search. Trying to full null issue.

--- src/main/java/com/project/Bookstore/BookDao.java
+++ src/main/java/com/project/Bookstore/BookDao.java
@@ -64,7 +64,7 @@ public class BookDao {
 
             if(rs.next()) {
                 result[0] = "" + rs.getInt(1);
-                for(int i = 1; i < 10; i++) {
+                for(int i = 1; i < result.length; i++) {
                     result[i] = rs.getString(i+1);
                 }
             }
--- src/main/java/com/project/Bookstore/SearchServlet.java
+++ src/main/java/com/project/Bookstore/SearchServlet.java
@@ -25,24 +25,32 @@ public class SearchServlet extends HttpServlet {
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/search.jsp");
         dispatcher.forward(request, response);
+        request.getSession().setAttribute("searchResult","");
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String search = request.getParameter("username");
+        String search = request.getParameter("search");
         String[] desp = new String[13];
-        PrintWriter writer = response.getWriter();
-        writer.print(search);
         try {
             desp = bookDao.findBook(search);
-            int i = 0;
-            String htmlRespone = "";
-            while (desp[i] != null && i < 13) {
-                htmlRespone += "<h2>" + desp[i] + "</h2><br>";
-                i++;
-            }
-            writer.println(htmlRespone);
+            String htmlResponse = "<html>";
+            htmlResponse += "<img src=\""+desp[4]+"\">";
+            htmlResponse += "<h3> Price: $" + desp[11] + "</h3><br>";
+            htmlResponse += "<h3> ISBN: " + desp[0] + "</h3><br>";
+            htmlResponse += "<h3> Genre: " + desp[1] + "</h3><br>";
+            htmlResponse += "<h3> Author: " + desp[2] + "</h3><br>";
+            htmlResponse += "<h3> Title: " + desp[3] + "</h3><br>";
+            htmlResponse += "<h3> Edition #: " + desp[5] + "</h3><br>";
+            htmlResponse += "<h3> Publisher: " + desp[6] + "</h3><br>";
+            htmlResponse += "<h3> Publication Year: " + desp[7] + "</h3><br>";
+            htmlResponse += "<h3> Books Left: " + desp[8] + "</h3><br>";
+            htmlResponse += "<h3> Trade-in Value: $" + desp[10] + "</h3><br>";
+            htmlResponse += "</html>";
+            request.getSession().setAttribute("searchResult",htmlResponse);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/search.jsp");
+        dispatcher.forward(request, response);
     }
 }
